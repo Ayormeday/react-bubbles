a9c763030cbe5f54bec05e4de005eada06ddbae9
Ayomide Jones <ayormeday@gmail.com>
2019-10-18 09:21:01 +0100

    bubble page wraped inprivate router and authcheck function created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, withRouter, Redirect } from "react-router-dom";
+import BubblePage from './components/BubblePage';
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -13,9 +13,22 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+
+        <Route path= "/bubblePage"
+          render={props => withAuthCheck(BubblePage, props)}
+        />
       </div>
     </Router>
   );
 }
+
+
+function withAuthCheck(Component, props) {
+  if (localStorage.getItem('token')) {
+    return <Component {...props} />
+  }
+  return <Redirect to='/' />;
+ }
+
 
 export default App;
